0d2f79462a216c99243c38fd7f60202206ebc549
Peter Ruan <pruan@redhat.com>
2018-12-21 14:58:47 -0800

    added installation of metering using shell script that comes with the… (#51)

--- features/step_definitions/admin.rb
+++ features/step_definitions/admin.rb
@@ -22,3 +22,23 @@ Given /^a secret is created for admin kubeconfig in current project$/ do
     raise "cannot create secret, see log"
   end
 end
+
+Given /^I save the project hosting #{QUOTED} resource named #{QUOTED} to#{OPT_QUOTED} clipboard/ do |resource, res_name, cb_name|
+  ensure_admin_tagged
+
+  cb_name ||= :namespace
+  @result = admin.cli_exec(:get, all_namespaces: true, resource: resource, o: 'yaml')
+  if @result[:parsed].nil?
+    logger.info("Can't find resource matching '#{resource} in any projects")
+    cb[cb_name] = nil
+  else
+    res = @result[:parsed]['items'].select { |i| i['metadata']['name']==res_name }
+    if res.count.zero?
+      logger.info("Can't find resource matching name '#{res_name} in any projects")
+      cb[cb_name] = nil
+    else
+      cb[cb_name] = project(res.first['metadata']['namespace'])
+    end
+  end
+end
+
