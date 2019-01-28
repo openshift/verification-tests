cd6bc2a54e8985598a65f5afe87e443b7b096a39
Yanping Zhang <yanpzhan@redhat.com>
2019-01-28 20:26:52 +0800

    Adjust admin console login (#57)

--- features/step_definitions/admin_console.rb
+++ features/step_definitions/admin_console.rb
@@ -2,30 +2,19 @@
 #   cb_name ||= :console
 #   cb[cb_name] = route('console', service('console',project('openshift-console', switch: false))).dns(by: admin)
 # end
-
 Given /^I open admin console in a browser$/ do
   base_rules = BushSlicer::WebConsoleExecutor::RULES_DIR + "/base/"
   snippets_dir = BushSlicer::WebConsoleExecutor::SNIPPETS_DIR
 
   version = env.webconsole_executor.get_master_version(user, via_rest: true)
-
   step "I have a browser with:", table(%{
     | rules        | #{base_rules}                            |
     | rules        | lib/rules/web/admin_console/base/        |
     | rules        | lib/rules/web/admin_console/#{version}/  |
-    | base_url     | <%= env.web_console_url %>               |
+    | base_url     | <%= env.admin_console_url %>             |
     | snippets_dir | #{snippets_dir}                          |
   })
-  @result = browser.run_action(:login,
-                               username: user.auth_name,
-                               password: user.password)
-  raise "cannot login to web console" unless @result[:success]
-  @result = browser.run_action(:click_console_selector)
-  raise "cannot click user dropdown menu" unless @result[:success]
-  # we need to open in same tab and I don't know a better way
-  # console_url = browser.browser.a(text: "Cluster Console").href
-  # browser.browser.goto console_url
-  browser.browser.a(text: "Cluster Console").click
+  browser.run_action(:goto_admin_console_root)
   @result = browser.run_action(:login_admin_console,
                                username: user.auth_name,
                                password: user.password)
