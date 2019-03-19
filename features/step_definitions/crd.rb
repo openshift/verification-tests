0af40da8637df33fa743b925bc338f07309c2250
Chuan Yu <chuyu@redhat.com>
2019-03-19 18:27:17 +0800

    add crd backup and restore steps

--- features/step_definitions/crd.rb
+++ features/step_definitions/crd.rb
@@ -1,11 +1,11 @@
 Given /^the #{QUOTED} #{QUOTED} CRD is recreated after scenario$/ do |name, crd|
   ensure_admin_tagged
-  allowed_crds = ["oauth", "kubeapiserver"]
+  allowed_crds = ["kubeapiserver"]
 
-  if allowed_crds.included? 'crd'
-    cb.cluster_resource_to_recreate = crd(name)
+  if allowed_crds.include? crd
+    cb.cluster_resource_to_recreate = self.send(crd, name)
     step 'hidden recreate cluster resource after scenario'
   else
-    raise 'No such crd CRD in the cluster'
+    raise "#{crd} not supported by this step"
   end
 end
