938b602f4b6dfcc69dab07a6ce27885d5a49148a
xiuwang <xiuwang@redhat.com>
2019-03-29 11:19:14 +0800

    Remove conditions for older cluster verisions

--- features/step_definitions/images/jenkins.rb
+++ features/step_definitions/images/jenkins.rb
@@ -1,11 +1,5 @@
 #Use this step definition file to create image-specific steps
 
-#This empty step will be removed once we can start updating the existing
-#jenkins cases to use the new OAuth method; this way there is no
-#'missing step' error here.
-Given /^I save the jenkins password of dc #{QUOTED} into the#{OPT_SYM} clipboard$/ do
-end
-
 Given /^I have an?( ephemeral| persistent)? jenkins v#{NUMBER} application(?: from #{QUOTED})?$/ do |type, version, cust_templ|
 
   if cust_templ && type
@@ -30,15 +24,14 @@ Given /^I have an?( ephemeral| persistent)? jenkins v#{NUMBER} application(?: fr
     source = "| template | jenkins-#{type.strip} |"
   end
 
-  lt34 = env.version_lt("3.4", user: user)
   last_startup_check = monotonic_seconds
 
-  # On v3.4 and above, if we can login to OpenShift via password, then
+  # If we can login to OpenShift via password, then
   #   we can use SSO login.
-  # On 3.3 and earlier, or if we only know user token, then we must use
+  # If we only know user token, then we must use
   #   non-SSO login.
   # Installation step also installs based on this.
-  if lt34 || !user.password?
+  if !user.password?
     step %Q/I run the :new_app client command with:/, table(%{
       #{source}
       | p | ENABLE_OAUTH=false                          |
@@ -60,17 +53,13 @@ Given /^I have an?( ephemeral| persistent)? jenkins v#{NUMBER} application(?: fr
   cb.jenkins_dns = cb.jenkins_route.dns
   cb.jenkins_major_version = version
 
-  # wait for actual startup as in 3.10+ it is slow on only 512M pod
-  if version == "1"
-    timeout = 300
-    wait_string = "Jenkins is fully up and running"
-  else
+    # wait for actual startup as in 3.10+ it is slow on only 512M pod
     # seems like recent jenkins has a delay caused by admin monitor
     # here trying to wait for it's operation to finish
     # possible remedy is DISABLE_ADMINISTRATIVE_MONITOR or give mor RAM to pod
-    timeout = 600
-    wait_string = "Finished Download metadata."
-  end
+  timeout = 600
+  wait_string = "Finished Download metadata."
+
   started = wait_for(timeout) {
     since = monotonic_seconds - last_startup_check
     res = user.cli_exec(
@@ -97,52 +86,33 @@ Given /^I have a jenkins browser$/ do
   step 'I have a browser with:', table(opts)
 end
 
-# On v3.4 and above, if we can login to OpenShift via password, then
+# If we can login to OpenShift via password, then
 #   we can use SSO login.
-# On 3.3 and earlier, or if we only know user token, then we must use
+# If we only know user token, then we must use
 #   non-SSO login.
-# Installation step also installs based on this.
 Given /^I log in to jenkins$/ do
-  if env.version_ge("3.4", user: user) && env.version_le("3.11", user: user) && user.password?
-    step %Q/I perform the :jenkins_oauth_login web action with:/, table(%{
-      | username | <%= user.name %>     |
-      | password | <%= user.password %> |
-      })
-  elsif env.version_gt("3.11", user: user) && user.password?
+  if env.version_gt("3.11", user: user) && user.password?
     step %Q/I perform the :jenkins_multi_oauth_login web action with:/, table(%{
       | username | <%= user.name %>     |
       | password | <%= user.password %> |
       })
-  else
+  elsif !user.password?
     step %Q/I perform the :jenkins_standard_login web action with:/, table(%{
       | username | admin    |
       | password | password |
       })
+  else
+    raise "Please switch to v3 branch to launch automation."
   end
   step 'the step should succeed'
 end
 
 Given /^I update #{QUOTED} slave image for jenkins #{NUMBER} server$/ do |slave_name,jenkins_version|
 
-  step 'I store master major version in the clipboard'
-
   if env.version_gt("3.11", user: user)
     print "Skipped, agent images don't need update above openshift 4.0"
-  elsif env.version_le("3.9", user: user) or jenkins_version == '1'
-    step %Q/I perform the :jenkins_update_cloud_image web action with:/, table(%{
-      | currentimgval | registry.access.redhat.com/openshift3/jenkins-slave-#{slave_name}-rhel7                          |
-      | cloudimage    | <%= product_docker_repo %>openshift3/jenkins-slave-#{slave_name}-rhel7:v<%= cb.master_version %> |
-      })
-  elsif slave_name == 'maven'
-    step %Q/I perform the :jenkins_update_cloud_image web action with:/, table(%{
-      | currentimgval | openshift3/jenkins-agent-maven-35-rhel7                                                     |
-      | cloudimage    | <%= product_docker_repo %>openshift3/jenkins-agent-maven-35-rhel7:v<%= cb.master_version %> |
-      })
-  else slave_name == 'nodejs'
-    step %Q/I perform the :jenkins_update_cloud_image web action with:/, table(%{
-      | currentimgval | openshift3/jenkins-agent-nodejs-8-rhel7                                                     |
-      | cloudimage    | <%= product_docker_repo %>openshift3/jenkins-agent-nodejs-8-rhel7:v<%= cb.master_version %> |
-      })
+  else
+    raise "Please switch to v3 branch to launch automation."
   end
 
   step 'the step should succeed'
