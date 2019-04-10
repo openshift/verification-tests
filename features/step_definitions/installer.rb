2ade187a5a044533ee34f7756f2e18283918ee0e
Peter Ruan <pruan@redhat.com>
2019-04-10 09:48:43 -0700

    Added support for cluster upgrade and check status (#145)

---
+++ features/step_definitions/installer.rb
@@ -0,0 +1,24 @@
+## installer related steps such as upgrade
+
+# required input: to_image in the calling step, which can be passed manually or set in the environment variable
+# UPGRADE_TARGET_VERSION
+Given /^I upgrade my cluster to:$/ do | table |
+  ensure_admin_tagged
+  _admin = admin
+  opts = opts_array_to_hash(table.raw)
+  opts[:force] = true
+  target_version = opts[:to_image]
+  # patch spec.upstream
+  upstream_url = opts[:upstream_url] if opts[:upstream_url]
+  upstream_url ||= "https://openshift-release.svc.ci.openshift.org/graph"
+  patch_json = {"spec": {"upstream":"#{upstream_url}"}}
+  patch_opts = {resource: "clusterversion", resource_name: "version", p: patch_json.to_json, type: "merge"}
+  @result = _admin.cli_exec(:patch, **patch_opts)
+  raise "Patch failed with #{@result[:response]}" unless @result[:success]
+  opts[:to_image] = cluster_version('version').initial_image_url + ":" + opts[:to_image]
+  @result = _admin.cli_exec(:oadm_upgrade, opts)
+  raise "Upgrade command failed #{@result[:response]}" unless @result[:success]
+  upgrade_status = cluster_version('version').wait_for_upgrade_completion(version: target_version)
+  raise "Upgrade to #{target_version} failed" unless upgrade_status
+end
+
--- lib/openshift/cluster_version.rb
+++ lib/openshift/cluster_version.rb
@@ -15,5 +15,55 @@ module BushSlicer
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       rr.dig('status', 'desired', 'version')
     end
+
+    def history(user: nil, cached: false, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('status', 'history')
+    end
+
+    # return a list of history matching the state we want to filter on
+    # query_hash is the parameter we want to filter on,
+    def history_matching(key: nil, value:nil , user: nil, cached: true, quiet: false)
+      self.history.select { |h| h[key] == value }
+    end
+
+    def image(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('status', 'desired', 'image')
+    end
+
+    # get the last entry of the spec.history which should be where the original
+    def initial_image_url(user: nil, cached: true, quiet: false)
+      self.history.last['image'].split('@sha256:')[0]
+    end
+
+    # @param version [String] upgrade version that we check for completion
+    def upgrade_completed?(version:, user: nil, cached: true, quiet: false)
+      res = self.history_matching(key: 'version', value: version)
+      if res.count > 0
+        logger.debug("MSG: #{res.first['message']}")
+        res.first['state'] == 'Completed'
+      else
+        false
+      end
+    end
+
+    def wait_for_upgrade_completion(version:, user: nil, quiet: false, timeout: 2*60*60)
+      wait_for(timeout) {
+        upgrade_completed?(version: version, user: user, cached: false, quiet: quiet)
+      }
+    end
+    # extract the percentage done from the message field from spec.conditions
+    # default to return nil if no upgarde is being done.
+    def completed_percentage(user: nil, cached: true, quiet: false)
+      progressing_cond = self.condition(type: 'Progressing')
+      percent_done = nil
+      if progressing_cond
+        percent_regexp = /(\d+)%/
+        found_percent = percent_regexp.match(progressing_cond['message'])
+        percent_done = found_percent[1].to_i if found_percent
+      end
+      percent_done
+    end
   end
 end
