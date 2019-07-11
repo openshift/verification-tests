f87b4cde3895a68492c4f4236cd70ae788d73944
Peter Ruan <pruan@redhat.com>
2019-07-11 01:28:06 -0700

    added related objects support in cluster_operator class

--- lib/openshift/cluster_operator.rb
+++ lib/openshift/cluster_operator.rb
@@ -11,5 +11,28 @@ module BushSlicer
       res = self.versions.select { |v| v == version }
       res.count != 0
     end
+    # @return an array of relatedObjects
+    #   relatedObjects:
+    #   - group: ""
+    #     name: openshift-cluster-storage-operator
+    #     resource: namespaces
+    #   - group: config.openshift.io
+    #     name: cluster
+    #     resource: infrastructures
+    #   - group: storage.k8s.io
+    #     name: gp2
+    #     resource: storageclasses
+    def related_objects(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('status', 'relatedObjects')
+    end
+
+    # @param filter_by [String] the hash key to filter by
+    # @param value [String] the value that we wish to match for selection
+    def related_object(filter_by:, value:, user: nil, cached: true, quiet: false)
+      ro = related_objects(user: user, cached: cached, quiet: quiet).select { |r|  r[filter_by] == value }
+      ro.first
+    end
+
   end
 end
