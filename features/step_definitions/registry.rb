773b4dc80f39fd983b9ccc56a8c9a4eac2535ef1
wzheng <wzheng@openshift-qe.desktop.redhat.cn>
2019-08-06 17:38:08 +0800

    remove namespace

--- features/step_definitions/registry.rb
+++ features/step_definitions/registry.rb
@@ -394,10 +394,10 @@ Given /^default image registry route is stored in the#{OPT_SYM} clipboard$/ do |
   project(org_proj_name) if org_proj_name
 end
 
-Given /^current generation number of#{OPT_QUOTED} deployment is stored into#{OPT_SYM} clipboard$/ do |name,cb_name|
+Given /^current generation number of#{OPT_QUOTED} deployment under #{QUOTED} is stored into#{OPT_SYM} clipboard$/ do |name,namespace,cb_name|
   ensure_admin_tagged
   ensure_destructive_tagged
   cb_name ||= :generation_number
-  @result = admin.cli_exec(:get, resource: 'deployment', resource_name: name, n: 'openshift-image-registry', o: 'yaml')
+  @result = admin.cli_exec(:get, resource: 'deployment', resource_name: name, n: namespace, o: 'yaml')
   cb[cb_name] = @result[:parsed]['metadata']['generation']
 end
