b7ac16b7288ebf2f3ed870fd34faffe1fbdf0fdc
Aleksandar Kostadinov <akostadi@redhat.com>
2019-08-19 02:03:05 +0300

    add config.samples.operator.openshift.io resource

---
+++ lib/openshift/config_samples_operator_openshift_io.rb
@@ -0,0 +1,7 @@
+require 'openshift/cluster_resource'
+
+module BushSlicer
+  class ConfigSamplesOperatorOpenshiftIo < ClusterResource
+    RESOURCE = 'configs.samples.operator.openshift.io'
+  end
+end
