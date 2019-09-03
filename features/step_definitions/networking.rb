e4275be00643a481098c152a4d637c500958f73a
zzhao <zzhao@dhcp-140-240.nay.redhat.com>
2019-09-03 15:14:22 +0800

    admin update

--- features/step_definitions/networking.rb
+++ features/step_definitions/networking.rb
@@ -38,12 +38,11 @@ end
 Given /^the env is using one of the listed network plugins:$/ do |table|
   ensure_admin_tagged
   plugin_list = table.raw.flatten
-  _admin = admin
+  #_admin = admin
 
-  @result = _admin.cli_exec(:get, resource: "clusternetwork", resource_name: "default", template: '{{.pluginName}}')
-  if @result[:success]
-    # only check stdout because stderr can contain "-" and cause the split to fail
-    plugin_name = @result[:stdout].to_s.split("-").last
+  @result = admin.cli_exec(:get, resource: "clusternetwork", resource_name: "default", template: '{{.pluginName}}')
+  if @result[:success] then
+    plugin_name = @result[:response].split("-").last
     unless plugin_list.include? plugin_name
       raise "the env network plugin is #{plugin_name} but expecting #{plugin_list}."
     end
