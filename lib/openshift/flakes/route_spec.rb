ab67a6aa7426b58e2dc46fd5dbacf9216bcedb6f
Aleksandar Kostadinov <akostadi@redhat.com>
2019-09-16 17:32:49 +0300

    code style and added methods

--- lib/openshift/flakes/route_spec.rb
+++ lib/openshift/flakes/route_spec.rb
@@ -4,8 +4,6 @@ module BushSlicer
 
   # this class should help with parsing route specifications
   class RouteSpec
-    include Common::Helper
-
     attr_reader :struct
     private :struct
 
@@ -13,46 +11,56 @@ module BushSlicer
       @struct = struct
     end
 
-    module ExportMethods
-      def  host
-        return struct['host']
-      end
-
-      def targetPort
-        return struct.dig('port', 'targetPort')
-      end
-
-      def subdomain
-        return struct['subdomain']
-      end
-
-      def tls_insecureEdgeTerminationPolicy
-        return struct.dig('tls', 'insecureEdgeTerminationPolicy')
-      end
-
-      def tls_termination
-        return struct.dig('tls', 'termination')
-      end
-
-      def to_kind
-        return struct.dig('to', 'kind')
-      end
-
-      def to_name
-        return struct.dig('to', 'name')
-      end
-
-      def to_weight
-        return struct.dig('to', 'weight')
-      end
-
-      def wildcardPolicy
-        return struct['wildcardPolicy']
-      end
-
+    def  host
+      return struct['host']
     end
 
-    include ExportMethods
+    def target_port
+      return struct.dig('port', 'targetPort')
+    end
 
+    def subdomain
+      return struct['subdomain']
+    end
+
+    def tls_insecure_edge_termination_policy
+      return struct.dig('tls', 'insecureEdgeTerminationPolicy')
+    end
+
+    def tls_termination
+      return struct.dig('tls', 'termination')
+    end
+
+    def tls_certificate
+      return struct.dig('tls', 'certificate')
+    end
+
+    def tls_key
+      return struct.dig('tls', 'key')
+    end
+
+    def tls_ca_certificate
+      return struct.dig('tls', 'caCertificate')
+    end
+
+    def tls_destination_ca_certificate
+      return struct.dig('tls', 'destinationCACertificate')
+    end
+
+    def to_kind
+      return struct.dig('to', 'kind')
+    end
+
+    def to_name
+      return struct.dig('to', 'name')
+    end
+
+    def to_weight
+      return struct.dig('to', 'weight')
+    end
+
+    def wildcard_policy
+      return struct['wildcardPolicy']
+    end
   end
 end
