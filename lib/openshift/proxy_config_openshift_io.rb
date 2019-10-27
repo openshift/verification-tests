56560740c3f33dbdf40a86da25b5313401826c19
Aleksandar Kostadinov <akostadinov@gmail.com>
2019-10-27 23:51:54 +0200

    fix style

--- lib/openshift/proxy_config_openshift_io.rb
+++ lib/openshift/proxy_config_openshift_io.rb
@@ -4,12 +4,12 @@ module BushSlicer
   class ProxyConfigOpenshiftIo < ClusterResource
     RESOURCE = 'proxies.config.openshift.io'
 
-    def httpproxy(user: nil, quiet: false, cached: true)
+    def http_proxy(user: nil, quiet: false, cached: true)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       return rr.dig('spec', 'httpProxy')
     end
 
-    def httpsproxy(user: nil, quiet: false, cached: false)
+    def https_proxy(user: nil, quiet: false, cached: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       return rr.dig('spec', 'httpsProxy')
     end
