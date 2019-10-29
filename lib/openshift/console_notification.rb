584423bd41ad0f3d4f59569793c7cf72657119b8
xiaocwan <xiaocwan@redhat.com>
2019-10-29 15:13:34 +0800

    Update resource defination to append resource group name as full name

--- lib/openshift/console_notification.rb
+++ lib/openshift/console_notification.rb
@@ -2,6 +2,6 @@ require 'openshift/cluster_resource'
 
 module BushSlicer
   class ConsoleNotification < ClusterResource
-    RESOURCE = "consolenotifications"
+    RESOURCE = "consolenotification.console.openshift.io"
   end
 end
