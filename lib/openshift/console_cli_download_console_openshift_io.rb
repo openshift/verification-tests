782afa7a40de7dc367317cb31e1542c358612bd2
xiaocwan <xiaocwan@redhat.com>
2019-10-31 14:22:40 +0800

    update rules for OCP-24287

--- lib/openshift/console_cli_download_console_openshift_io.rb
+++ lib/openshift/console_cli_download_console_openshift_io.rb
@@ -2,7 +2,7 @@ require 'openshift/cluster_resource'
 
 module BushSlicer
   # @note represents an OpenShift cluster resource about console
-  class ConsoleclidownloadConsoleOpenshiftIo < ClusterResource
-    RESOURCE = 'consoleclidownload.console.openshift.io'
+  class ConsoleCliDownloadConsoleOpenshiftIo < ClusterResource
+    RESOURCE = 'console.cli.download.console.openshift.io'
   end
 end
