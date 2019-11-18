811911014a383629fec04552d5f525aff0552a77
xiaocwan <xiaocwan@redhat.com>
2019-11-18 14:40:56 +0800

    update

--- lib/openshift/console_notification_console_openshift_io.rb
+++
@@ -1,7 +0,0 @@
-require 'openshift/cluster_resource'
-
-module BushSlicer
-  class ConsoleNotificationConsoleOpenshiftIo < ClusterResource
-    RESOURCE = "consolenotification.console.openshift.io"
-  end
-end
