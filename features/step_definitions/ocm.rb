3a0d6bce989b0f8cd485a5ea51776265b8c10bc6
yuwan <yuwan@redhat.com>
2019-11-26 15:03:47 +0800

    add one step definition/some rules for ocm login and add the env config for ocm UI testing

---
+++ features/step_definitions/ocm.rb
@@ -0,0 +1,20 @@
+Given /^I open ocm portal with #{WORD}$/ do |usertype|
+  base_rules = BushSlicer::WebConsoleExecutor::RULES_DIR
+  snippets_dir = BushSlicer::WebConsoleExecutor::SNIPPETS_DIR
+  portals = YAML.load_file(expand_private_path("config/credentials/ocm.yaml"))
+  ocm_env = env.ocm_env
+  base_url = env.web_console_url
+  step "I have a browser with:", table(%{
+    | rules        | lib/rules/web/ocm_console/  |
+    | base_url     | #{base_url}       |
+    | snippets_dir | #{snippets_dir}                    |
+  })
+  if ocm_env == "stage"
+    browser.run_action(:goto_ocm_stage_console)
+  elsif ocm_env == "product"
+    browser.run_action(:goto_ocm_prod_console)
+  end
+  @result = browser.run_action(:login_ocm_sequence,
+                               username: portals[ocm_env]["users"][usertype]["username"],
+                               password: portals[ocm_env]["users"][usertype]["password"])
+end
