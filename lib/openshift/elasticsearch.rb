c227924c89f1ac1d8e09235f9a916449c6da464b
qitang <qitang@redhat.com>
2019-12-17 13:19:20 +0800

    add methods and lib files for logging cases

--- lib/openshift/elasticsearch.rb
+++ lib/openshift/elasticsearch.rb
@@ -1,3 +1,5 @@
+require 'openshift/project_resource'
+
 module BushSlicer
   class Elasticsearch < ProjectResource
     RESOURCE = "elasticsearches"
@@ -15,6 +17,32 @@ module BushSlicer
     def nodes(user: nil, quiet: false, cached: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       return rr.dig('spec', 'nodes')
+    end
+
+    def resources(user: nil, quiet: true, cached: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      return rr.dig('spec', 'nodeSpec', 'resources')
+    end
+
+    def resource_limit_cpu(user: nil, quiet: true, cached: false)
+      return resources(user: user, cached: cached, quiet: quiet).dig('limits', 'cpu')
+    end
+
+    def resource_limit_memory(user: nil, quiet: true, cached: false)
+      return resources(user: user, cached: cached, quiet: quiet).dig('limits', 'memory')
+    end
+
+    def resource_request_cpu(user: nil, quiet: true, cached: false)
+      return resources(user: user, cached: cached, quiet: quiet).dig('requests', 'cpu')
+    end
+
+    def resource_request_memory(user: nil, quiet: true, cached: false)
+      return resources(user: user, cached: cached, quiet: quiet).dig('requests', 'memory')
+    end
+
+    def node_selector(user: nil, quiet: true, cached: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      return rr.dig('spec', 'nodeSpec', 'nodeSelector')
     end
 
   end
