1ab2a0e007fc5802c0a1f762bc54057801ec20f5
wzheng <wzheng@redhat.com>
2019-12-23 13:51:50 +0800

    fix

--- vendor/bundle/ruby/2.5.0/gems/atlassian-jwt-0.2.0/lib/atlassian/jwt/version.rb
+++
@@ -1,5 +0,0 @@
-module Atlassian
-  module Jwt
-    VERSION = "0.2.0"
-  end
-end
--- vendor/bundle/ruby/2.5.0/gems/aws-sdk-core-2.11.250/lib/aws-sdk-core/guardduty.rb
+++
@@ -1,5 +0,0 @@
-Aws.add_service(:GuardDuty, {
-  api: "#{Aws::API_DIR}/guardduty/2017-11-28/api-2.json",
-  docs: "#{Aws::API_DIR}/guardduty/2017-11-28/docs-2.json",
-  paginators: "#{Aws::API_DIR}/guardduty/2017-11-28/paginators-1.json",
-})
