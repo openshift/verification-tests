e5d20b71037234ef42905cf1100cc8d037bd232c
xiuwang <xiuwang@redhat.com>
2019-12-26 16:16:33 +0800

    Auto OCP-18994 and OCP-18998

--- features/step_definitions/registry.rb
+++ features/step_definitions/registry.rb
@@ -397,3 +397,44 @@ Given /^current generation number of#{OPT_QUOTED} deployment is stored into#{OPT
   cb_name ||= :generation_number
   cb[cb_name] = deployment(name).generation_number(user: user, cached: false)
 end
+
+Given /^docker config for default image registry is saved$/ do
+  step %Q/I enable image-registry default route/
+  step %Q/default image registry route is stored in the :integrated_reg_ip clipboard/
+  step %Q/a 5 characters random string of type :dns is stored into the :dockercfg_name clipboard/
+  cb.dockercfg_file="/tmp/#{cb.dockercfg_name}"
+
+  generated_cfg={
+        "#{cb.integrated_reg_ip}" => {
+          "auth" => Base64.strict_encode64(
+            "#{user.name}:#{user.cached_tokens.first}"
+          ),
+        } 
+  }
+
+  File.open("#{cb.dockercfg_file}", 'wb') { |f| 
+    f.write(
+       {   
+         "auths" => generated_cfg
+       }.to_json
+    )
+  }
+end
+
+Given /^certification for default image registry is saved$/ do
+  ensure_admin_tagged
+  
+  step %Q/a 5 characters random string of type :dns is stored into the :regcrt clipboard/
+  cb.reg_crt_name="/tmp/#{cb.regcrt}.crt"
+
+  org_user = @user
+  org_proj_name = project.name
+  @user = admin
+  @result = secret("router-certs-default", project("openshift-ingress", switch: false)).value_of("tls.crt")
+  File.open("#{cb.reg_crt_name}", 'wb') { |f| 
+    f.write(@result)
+  }
+
+  @user = org_user
+  project(org_proj_name)
+end
