3c77571b3e28712b65059b2e530e78f624dd6e1b
Han Sha <hasha@redhat.com>
2020-01-06 17:53:28 +0800

    update

--- lib/openshift/console_external_log_links_console_openshift_io.rb
+++
@@ -1,7 +0,0 @@
-require 'openshift/cluster_resource'
-
-module BushSlicer
-  class ConsoleExternalLogLinksConsoleOpenshiftIo < ClusterResource
-    RESOURCE = 'consoleexternalloglinks.console.openshift.io'
-  end
-end
