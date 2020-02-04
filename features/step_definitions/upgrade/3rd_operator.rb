3e995d7ec48a0363f7de8f6b9b1a17a33869794a
Jian Zhang <jiazha@redhat.com>
2020-02-04 14:47:48 +0800

    remove some unnecessary steps

---
+++ features/step_definitions/upgrade/3rd_operator.rb
@@ -0,0 +1,96 @@
+Given /^etcd operator "([^"]*)" is removed successfully from "([^"]*)" project$/ do | name, proj_name|
+  ensure_admin_tagged
+  raise "!!! Error doesn't exist etcd operator: #{name} in this #{proj_name} project" unless subscription("#{name}").exists?
+  step %Q/I ensures "#{name}" subscription is deleted from the "#{proj_name}" project/
+
+  step %Q/I run the :delete client command with:/, table(%{
+    | object_type       | clusterserviceversion |
+    | all               |                       |
+    | n                 | #{proj_name}          |
+  })
+  raise "Error removing CSV: #{name} in #{proj_name} project" unless @result[:success]
+
+  step %Q/I ensures "etcd-operator" deployment is deleted from the "#{proj_name}" project/
+  logger.info("### etcd operator: #{name} is removed successfully from #{proj_name} namespace")
+
+end
+
+Given /^etcd operator "([^"]*)" is installed successfully in "([^"]*)" project$/ do | name, proj_name|
+    ensure_admin_tagged
+    step %Q/I switch to cluster admin pseudo user/
+    step %Q/I use the "#{proj_name}" project/
+    unless operator_group('etcd-og').exists?
+      # Create operator group in this namespace
+      operator_group_yaml ||= "https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/olm/operatorgroup-template.yaml"
+      step %Q/I process and create:/, table(%{
+        | f | #{operator_group_yaml} |
+        | p | NAME=etcd-og         |
+        | p | NAMESPACE=#{proj_name} |
+      })
+      raise "Error creating OperatorGroup: etcd-og" unless @result[:success]
+    end
+    logger.info("### operator group: etcd-og is installed successfully in #{proj_name} namespace")
+
+    unless subscription("#{name}").exists?
+      # Subscribe etcd operator
+      sub_etcd_yaml ||= "https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/olm/etcd-subscription-template.yaml"
+      step %Q/I process and create:/, table(%{
+        | f | #{sub_etcd_yaml}                      |
+        | p | NAME=#{name}                          |
+        | p | NAMESPACE=#{proj_name}                |
+        | p | SOURCENAME=community-operators        |
+        | p | SOURCENAMESPACE=openshift-marketplace |     
+      })
+      raise "Error creating subscription: #{name}" unless @result[:success]
+    end
+
+    step %Q/I wait for the "etcd-operator" deployment to appear/
+    step %Q/a pod becomes ready with labels:/, table(%{
+      | name=etcd-operator-alm-owned |
+    })
+    raise "Error creating #{name} pods in #{proj_name} project" unless @result[:success]
+    
+    step %Q/I run the :get client command with:/, table(%{
+      | resource | etcdcluster |                                                  
+    })
+    step %Q/the output should contain "No resources found"/
+    raise "Error find the etcdCluster resource in #{proj_name} project" unless @result[:success]
+    logger.info("### etcd operator: #{name} is installed successfully in #{proj_name} namespace")
+end
+
+Given /^etcdCluster "([^"]*)" is installed successfully in "([^"]*)" project$/ do | name, proj_name|
+  step %Q/I use the "#{proj_name}" project/
+  etcdCluster_yaml ||= "https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/olm/etcd-cluster-template.yaml"
+  step %Q/I process and create:/, table(%{
+    | f | #{etcdCluster_yaml} |
+    | p | NAME=#{name}         |
+    | p | NAMESPACE=#{proj_name} |
+  })
+  raise "Error creating etcdCluster: #{name} in project: #{proj_name}" unless @result[:success]
+  logger.info("### etcdCluster: #{name} is installed successfully in #{proj_name} namespace")
+end
+
+Given /^etcdCluster "([^"]*)" is removed successfully from "([^"]*)" project$/ do | name, proj_name|
+  step %Q/I ensure "#{name}" etcdcluster is deleted from the "#{proj_name}" project/
+  logger.info("### etcdCluster: #{name} is removed successfully from #{proj_name} namespace")
+end
+
+Given /^the status of condition "([^"]*)" for "([^"]*)" operator is: (.+)$/ do | type, operator, status |
+  ensure_admin_tagged
+  actual_status = cluster_operator(operator).condition(type: type, cached: false)['status']
+  unless status == actual_status
+    raise "status of #{operator} condition #{type} is #{actual_status}"
+  end
+end
+
+Given /^the "([^"]*)" operator version matchs the current cluster version$/ do | operator |
+  ensure_admin_tagged
+  @result = admin.cli_exec(:get, resource: "clusteroperators", resource_name: operator, o: "jsonpath={.status.versions[?(.name == \"operator\")].version}")
+  operator_version = @result[:response]
+
+  @result = admin.cli_exec(:get, resource: "clusterversion", resource_name: "version", o: "jsonpath={.status.desired.version}")
+  cluster_version = @result[:response]
+
+  raise "The #{operator} version doesn't match the current cluster version" unless operator_version == cluster_version
+  logger.info("### the cluster version is #{cluster_version}")
+end
--- features/upgrade/olm/upgrade.feature
+++ features/upgrade/olm/upgrade.feature
@@ -8,12 +8,12 @@ Feature: OLM related scenarios
   @users=upuser1,upuser2
   Scenario: upgrade OLM - prepare
     # Check OLM version
-    Given The "operator-lifecycle-manager" operator version matchs the current cluster version
+    Given the "operator-lifecycle-manager" operator version matchs the current cluster version
     # Check cluster operator OLM status
-    Given The status of condition "Degraded" for "operator-lifecycle-manager" operator is: False
-    Given The status of condition "Progressing" for "operator-lifecycle-manager" operator is: False
-    Given The status of condition "Available" for "operator-lifecycle-manager" operator is: True
-    Given The status of condition "Upgradeable" for "operator-lifecycle-manager" operator is: True
+    Given the status of condition "Degraded" for "operator-lifecycle-manager" operator is: False
+    Given the status of condition "Progressing" for "operator-lifecycle-manager" operator is: False
+    Given the status of condition "Available" for "operator-lifecycle-manager" operator is: True
+    Given the status of condition "Upgradeable" for "operator-lifecycle-manager" operator is: True
     # Create a namespace and an operator in it
     Given I switch to cluster admin pseudo user
     When I run the :new_project client command with:
@@ -27,12 +27,12 @@ Feature: OLM related scenarios
   @users=upuser1,upuser2
   Scenario: upgrade OLM
     # Check OLM version after upgraded
-    Given The "operator-lifecycle-manager" operator version matchs the current cluster version
+    Given the "operator-lifecycle-manager" operator version matchs the current cluster version
     # Check cluster operator OLM status
-    Given The status of condition "Degraded" for "operator-lifecycle-manager" operator is: False
-    Given The status of condition "Progressing" for "operator-lifecycle-manager" operator is: False
-    Given The status of condition "Available" for "operator-lifecycle-manager" operator is: True
-    Given The status of condition "Upgradeable" for "operator-lifecycle-manager" operator is: True
+    Given the status of condition "Degraded" for "operator-lifecycle-manager" operator is: False
+    Given the status of condition "Progressing" for "operator-lifecycle-manager" operator is: False
+    Given the status of condition "Available" for "operator-lifecycle-manager" operator is: True
+    Given the status of condition "Upgradeable" for "operator-lifecycle-manager" operator is: True
     # Check if this operator works well by changing its customer resource
     Given I switch to cluster admin pseudo user
     And I use the "olm-upgrade" project 
@@ -42,15 +42,9 @@ Feature: OLM related scenarios
       | p             | {"spec": {"size": 4 }} |
       | type          | merge                  |
     Then the step should succeed
-    Then I wait up to 180 seconds for the steps to pass:
-    """
-    And I run the :get client command with:
-      | resource | pods                        |
-      | l        | etcd_cluster=sample-cluster |
-    Then the output should match 4 times:
-      | sample-cluster.* | 
-    """
-    # Clean this operator and its resource
+    Given status becomes :succeeded of exactly 4 pods labeled:
+      | etcd_cluster=sample-cluster |
+    Then the step should succeed
     Given etcdCluster "sample-cluster" is removed successfully from "olm-upgrade" project
     Given etcd operator "etcd-test" is removed successfully from "olm-upgrade" project
     # This operator can be re-installed succefully
