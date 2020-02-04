e7ff421964e0563f386ec5f9d9220b9d743f0ed0
Aleksandar Kostadinov <akostadi@redhat.com>
2020-02-05 00:52:05 +0200

    fix non-conventional uppercase

--- features/step_definitions/operators.rb
+++ features/step_definitions/operators.rb
@@ -37,3 +37,15 @@ Given /^the "([^"]*)" operator version matchs the current cluster version$/ do |
   raise "The #{operator} version doesn't match the current cluster version" unless operator_version == cluster_version
   logger.info("### the cluster version is #{cluster_version}")
 end
+
+Given /^admin updated the operator crd "([^"]*)" managementstate operand to (Managed|Removed|Unmanaged)$/ do |cluster_operator, manage_type|
+  ensure_admin_tagged
+  ensure_destructive_tagged
+  step %Q/I run the :patch admin command with:/, table(%{
+    | resource      | #{cluster_operator}.operator.openshift.io      |
+    | resource_name | cluster                                        |
+    | p             | {"spec":{"managementState": "#{manage_type}"}} |
+    | type          | merge                                          |
+  })
+  step %Q/the step should succeed/
+end
