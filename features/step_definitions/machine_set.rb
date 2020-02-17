04635d5cf1fe902bb9132361b02b8db72ed5bf2e
Jianwei Hou <jhou1@users.noreply.github.com>
2020-02-17 23:53:47 +0800

    Ensure machine/node consistency after scenario (#713)
    * add machine phase
    
    * Ensure machine/node consistency after scenarioe

--- features/step_definitions/machine_set.rb
+++ features/step_definitions/machine_set.rb
@@ -32,14 +32,30 @@ end
 Then(/^the machineset should have expected number of running machines$/) do
   machine_set.wait_till_ready(admin, 600)
 
-  machines_all = BushSlicer::Machine.list(user: admin, project: project("openshift-machine-api"))
-  machines = machines_all.select { |m| m.machine_set_name == machine_set.name }
+  machines = BushSlicer::Machine.list(user: admin, project: project("openshift-machine-api"))
 
-  # Each node should be running
+  num_running_machines = 0
   machines.each do | machine |
+    next if machine.machine_set_name != machine_set.name
+
+    # if machine phase is 'Deleting' then wait for its node to disappear
+    if machine.phase == 'Deleting'
+      step %Q{I wait for the resource "node" named "#{machine.node_name}" to disappear within 600 seconds}
+      step %Q{I wait for the resource "machine" named "#{machine.name}" to disappear within 600 seconds}
+      next
+    end
+    
+    # wait till machine's node is ready
     machine.get
     unless node(machine.node_name).ready?[:success]
-      raise "Node #{machine.node_name} has not become ready."
+      raise "Node #{machine.node_name} has not become ready"
     end
+
+    num_running_machines+=1
+  end
+
+  available_replicas = machine_set.available_replicas(user: nil, cached: false, quiet: false)
+  if available_replicas != num_running_machines
+    raise "Machineset #{machine_set.name} has #{num_running_machines} running machines, expected #{available_replicas}"
   end
 end
