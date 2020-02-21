2c00850e336fe278371e4861ffb839a1e84ce372
tzhou <tzhou@localhost.localdomain>
2020-02-21 15:49:01 +0800

    fix the conflict

--- features/step_definitions/ocm.rb
+++ features/step_definitions/ocm.rb
@@ -1,20 +1,14 @@
-Given /^I open ocm portal with #{WORD}$/ do |usertype|
+Given /^I open ocm portal as an? #{WORD} user$/ do |usertype|                                                                                                                
   base_rules = BushSlicer::WebConsoleExecutor::RULES_DIR
   snippets_dir = BushSlicer::WebConsoleExecutor::SNIPPETS_DIR
-  portals = YAML.load_file(expand_private_path("config/credentials/ocm.yaml"))
-  ocm_env = env.ocm_env
   base_url = env.web_console_url
   step "I have a browser with:", table(%{
-    | rules        | lib/rules/web/ocm_console/  |
-    | base_url     | #{base_url}       |
-    | snippets_dir | #{snippets_dir}                    |
+    | rules        | lib/rules/web/ocm_console/ |
+    | base_url     | #{base_url}                |
+    | snippets_dir | #{snippets_dir}            |
   })
-  if ocm_env == "stage"
-    browser.run_action(:goto_ocm_stage_console)
-  elsif ocm_env == "product"
-    browser.run_action(:goto_ocm_prod_console)
-  end
+  browser.browser.goto base_url
   @result = browser.run_action(:login_ocm_sequence,
-                               username: portals[ocm_env]["users"][usertype]["username"],
-                               password: portals[ocm_env]["users"][usertype]["password"])
+                               username: env.static_user(usertype).loginname,
+                               password: env.static_user(usertype).password)
 end
