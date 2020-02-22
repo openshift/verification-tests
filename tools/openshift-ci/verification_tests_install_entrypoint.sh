c1409182dc7a87c75d270a51e43a847f252d2cfd
Aleksandar Kostadinov <akostadi@redhat.com>
2020-02-22 16:00:44 +0200

    unify all git clone operations

--- tools/openshift-ci/verification_tests_install_entrypoint.sh
+++ tools/openshift-ci/verification_tests_install_entrypoint.sh
@@ -5,12 +5,14 @@
 # see example install-variables-example.sh
 
 export WORKSPACE=`pwd`/flexy
+export BUSHSLICER_VMINFO_YAML="$WORKSPACE"/vminfo.yml
+export BUSHSLICER_HOSTS_SPEC_FILE="$WORKSPACE"/host.spec
 
 case "$1" in
 
   "destroy")
     cd "$WORKSPACE"
-    tools/launch_instance.rb terminate vminfo.yml
+    tools/launch_instance.rb terminate "$BUSHSLICER_VMINFO_YAML"
     exit 0
     ;;
 
@@ -29,50 +31,51 @@ case "$1" in
     ;;
 esac
 
+function git_clone() {
+  local git_var_uri git_dir git_var_branch git_var_git_crypt \
+        git_var_prefix git_branch git_crypt_key_file
+  git_var_uri="$1"
+  git_var_prefix="${git_var_uri%_URI}"
+  git_dir="${git_var_prefix#GIT_}"
+  git_dir="${git_dir,,}"
+  git_dir="${git_dir//_/-}"
+
+  git_var_branch="${git_var_prefix}_BRANCH"
+  if [[ "${!git_var_branch}" ]]; then
+    git_branch="--branch ${!git_var_branch}"
+  fi
+  # TODO: read variables for athentication
+  git clone --depth 1 --single-branch $git_branch "${!git_var_uri}" $git_dir
+
+  set +x
+  git_var_git_crypt="${git_var_prefix}_GIT_CRYPT"
+  if [[ "${!git_var_git_crypt}" ]]; then
+    cd $git_dir
+    git_crypt_key_file=`mktemp`
+    printf "${!git_var_git_crypt}" | base64 --decode > "$git_crypt_key_file"
+    git crypt unlock "$git_crypt_key_file"
+    rm -f "$git_crypt_key_file"
+    unset $git_var_git_crypt # unset var to avoid content leaking in the logs
+    cd ..
+  fi
+  set -x
+}
 
 # First clone verification-tests repo itself
-# TODO: read variables to allow different fork or branch of it
-FLEXY_URI="https://github.com/openshift/verification-tests.git"
-git clone --depth 1 --single-branch "$FLEXY_URI" "$WORKSPACE"
+: ${FLEXY_URI:=https://github.com/openshift/verification-tests.git}
+git_clone FLEXY_URI
 cd "$WORKSPACE"
 
 # Go over all GIT_xyz_URI environment variables and clone them
 for git_var in "${!GIT_@}"; do
-  # TODO: read variables for athentication
   if [[ "$git_var" == *_URI ]]; then
-    git_name="${git_var#GIT_}"
-    git_name="${git_name%_URI}"
-    git_dir="${git_name,,}"
-    git_dir="${git_dir//_/-}"
-
-    git_branch_var="GIT_${git_name}_BRANCH"
-    if [[ "${!git_branch_var}" ]]; then
-      git_branch="--branch ${!git_branch_var}"
-    fi
-    git clone --depth 1 --single-branch $git_branch "${!git_var}" $git_dir
-    unset git_branch
-
-    set +x
-    git_crypt_var="GIT_${git_name}_GIT_CRYPT"
-    if [[ "${!git_crypt_var}" ]]; then
-      cd $git_dir
-      git_crypt_key_file=`mktemp`
-      printf "${!git_crypt_var}" | base64 --decode > "$git_crypt_key_file"
-      git crypt unlock "$git_crypt_key_file"
-      rm -f "$git_crypt_key_file"
-      unset $git_crypt_var
-      cd ..
-    fi
-    set -x
+    git_clone $git_var
   fi
 done
 
-
-# TODO: sed output to filter out printig of _CREDENTIALS and _PASSWORD variables
-export BUSHSLICER_HOSTS_SPEC_FILE="$WORKSPACE"/host.spec
-export BUSHSLICER_VMINFO_YAML="$WORKSPACE"/vminfo.yml
 rm -f "$BUSHSLICER_HOSTS_SPEC_FILE" "$BUSHSLICER_VMINFO_YAML"
 ruby --version
 bundle check --gemfile=tools/Gemfile || tools/hack_bundle.rb
+# TODO: sed output to filter out printig of _CREDENTIALS and _PASSWORD variables
 bash -l -c "ruby tools/launch_instance.rb template -c '${VARIABLES_LOCATION}' -l '${INSTANCE_NAME_PREFIX}'"
 
