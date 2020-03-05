cd58245a3aa820dad37d2610cb2a40657f0b2ddd
Ross Brattain <rbrattai@redhat.com>
2020-03-04 21:26:34 -0500

    return pod directly

--- features/step_definitions/networking.rb
+++ features/step_definitions/networking.rb
@@ -951,6 +951,6 @@ Given /^I store the ovnkube-master#{OPT_QUOTED} leader pod in the#{OPT_SYM} clip
     pod.node_name == leader_node || pod.ip == leader_node
   }.first
   cache_resources leader_pod
-  cb[cb_leader_name] = leader_pod.name
+  cb[cb_leader_name] = leader_pod
 end
 
