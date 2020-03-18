8aef8a345578cdd4a3efc911adb91ab3dfb43909
Yanping Zhang <yanpzhan@redhat.com>
2020-03-19 02:51:28 +0800

    update idp script (#817)

--- lib/openshift/o_auth.rb
+++ lib/openshift/o_auth.rb
@@ -1,3 +1,4 @@
+require 'openshift/cluster_resource'
 module BushSlicer
   class OAuth < ClusterResource
     RESOURCE = "oauths"
@@ -13,12 +14,19 @@ module BushSlicer
     #  "htpasswd-bar"=>"htpasswd-8zlqs",
     #  "foobar"=>"htpasswd-txc9w"
     #}
+   
     def htpasswds(user: nil, cached: true, quiet: false)
       idps = raw_resource(user: user, cached: cached, quiet: quiet).dig('spec', 'identityProviders')
       # call .compact to remove `nil` compoents (type != 'HTPasswd')
       passwds = idps.map {|i| {i.dig('name') => i.dig('htpasswd', 'fileData', 'name')} if i['type'] == 'HTPasswd' }.compact
       # covert array of hashes to just Hash
       passwds.inject(:merge)
+    end
+    def idp(user: nil, name:, cached: true, quiet: false)
+      idps = raw_resource(user: user, cached: cached, quiet: quiet).dig('spec', 'identityProviders')
+      c = idps.find { |c|
+        c.dig('name') == name
+      } or raise "No idp with name #{name} found."
     end
 
   end
