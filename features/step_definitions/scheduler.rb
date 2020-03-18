74e8cf8bbe8c37db8c71719f941b9deaf6b12bba
Kasturi Narra <knarra@redhat.com>
2020-03-18 20:31:17 +0530

    Modified the func for teardown

--- features/step_definitions/scheduler.rb
+++ features/step_definitions/scheduler.rb
@@ -1,6 +1,8 @@
 Given /^the #{QUOTED} scheduler priorityclasses is restored after scenario$/ do |name|
-  ensure_admin_tagged
-  ensure_destructive_tagged
-  @result = admin.cli_exec(:delete, object_type: 'priorityclasses', object_name_or_id: name)
-  raise "priority classes were not deleted successfully!" unless @result[:success]
+  _admin = admin
+  teardown_add {
+    opts = {object_type: 'priorityclasses', object_name_or_id: name}
+    @result = _admin.cli_exec(:delete, **opts)
+    raise "Cannot delete priorityclass: #{name}" unless @result[:success]
+  }
 end
