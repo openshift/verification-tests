0e5956c72213776416260dc8f2aafffd57bf85de
miyadav <miyadav@redhat.com>
2020-04-23 11:16:10 +0530

    Update to handle instanceState node changed to vmState in Azure clusters
    shorten for readability
    
    indent to left

--- lib/openshift/machine.rb
+++ lib/openshift/machine.rb
@@ -27,8 +27,9 @@ module BushSlicer
     end
 
     def instance_state(user: nil, cached: true, quiet: false)
-      raw_resource(user: user, cached: cached, quiet: quiet).
-          dig('status', 'providerStatus', 'instanceState')
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('status', 'providerStatus', 'instanceState') ||
+      rr.dig('status', 'providerStatus', 'vmState')
     end
 
     def annotation_instance_state(user: nil, cached: true, quiet: false)
