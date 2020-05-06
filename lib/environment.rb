7f5989c42987c9585008b0d4441c93ec4417d8fa
Liang Xia <lxia@redhat.com>
2020-05-07 05:45:34 +0800

    Get version info from ClusterVersion (#923)
    * Get version info from ClusterVersion
    
    * Address comment from Alex

--- lib/environment.rb
+++ lib/environment.rb
@@ -278,11 +278,14 @@ module BushSlicer
     # @return [Array<String>] raw version, major and minor number
     def get_version(user:)
       if opts[:version]
-        @major_version, @minor_version = parse_version(opts[:version])
-        return opts[:version], @major_version, @minor_version
+        _version = opts[:version]
+      elsif admin?
+        _version = admin.cli_exec(:get, resource: "clusterversion", resource_name: "version", o: "jsonpath={.status.desired.version}")
+      else
+        raise "cluster version not set and getting without admin access not possible presently"
       end
-
-      raise "getting version of cluster as user not possible presently"
+      @major_version, @minor_version = parse_version(_version)
+      return _version, @major_version, @minor_version
 
       # obtained = user.rest_request(:version)
       # if obtained[:request_opts][:url].include?("/version/openshift") &&
