0bc335a976e192b6bb7dbce1f7a36610c1ddbe09
QiaolingTang <qitang@redhat.com>
2020-05-09 15:38:34 +0800

    add and update logging crd lib files

--- lib/openshift/elasticsearch.rb
+++ lib/openshift/elasticsearch.rb
@@ -14,6 +14,36 @@ module BushSlicer
       return rr.dig('spec', 'redundancyPolicy')
     end
 
+    def index_management(user: nil, quiet: false, cached: false)
+      return raw_resource(user: user, cached: cached, quiet: quiet).dig('spec', 'indexManagement')
+    end
+
+    def index_management_mappings(user: nil, quiet: false, cached: false)
+      return index_management(user: user, cached: cached, quiet: quiet).dig('mappings')
+    end
+
+    def mapping(user: nil, name:, quiet: false, cached: false)
+      mapping = nil
+      mappings = self.index_management_mappings(user: user, cached: cached, quiet: quiet)
+      mappings.each do | m |
+        mapping = m if m['name'] == name
+      end
+      return mapping
+    end
+
+    def index_management_policies(user: nil, quiet: false, cached: false)
+      return index_management(user: user, cached: cached, quiet: quiet).dig('policies')
+    end
+
+    def policy(user: nil, name:, quiet: false, cached: false)
+      policy = nil
+      policies = self.index_management_policies(user: user, cached: cached, quiet: quiet)
+      policies.each do | p |
+        policy = p if p['name'] == name
+      end
+      return policy
+    end
+
     def nodes(user: nil, quiet: false, cached: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       return rr.dig('spec', 'nodes')
@@ -45,9 +75,17 @@ module BushSlicer
       return rr.dig('spec', 'nodeSpec', 'nodeSelector')
     end
 
+    def status(user: nil, quiet: true, cached: false)
+      return raw_resource(user: user, cached: cached, quiet: quiet).dig('status')
+    end
+
     def cluster_status(user: nil, quiet: false, cached: false)
-      rr = raw_resource(user: user, cached: cached, quiet: quiet)
-      return rr.dig('status', 'cluster')
+      return status.dig('cluster')
+    end
+
+    def cluster_health(user: nil, cached: true, quiet: false)
+      return cluster_status(user: user, cached: true, quiet: quiet).dig('status') || 
+        status(user: user, cached: cached, quiet: quiet).dig('clusterHealth')   
     end
 
   end
