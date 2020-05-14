e8d0db665d5c5a98616c7e64518bc5608b96764b
Wenjing Zheng <wzheng@redhat.com>
2020-05-14 10:55:46 +0800

    Add image pruners (#1087)

---
+++ lib/openshift/imagepruners_imageregistry_operator_openshift_io.rb
@@ -0,0 +1,7 @@
+require 'openshift/cluster_resource'
+
+module BushSlicer
+  class ImageprunersImageregistryOperatorOpenshiftIo < ClusterResource
+    RESOURCE = 'imagepruners.imageregistry.operator.openshift.io' 
+  end
+end
