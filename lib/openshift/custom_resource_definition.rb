971e2a8a7d360b46434629fda0f4283138243f7f
Ya Dan Pei <yapei@redhat.com>
2020-05-28 02:13:05 +0800

    add rules for OCP-25803 (#1155)

--- lib/openshift/custom_resource_definition.rb
+++ lib/openshift/custom_resource_definition.rb
@@ -3,5 +3,20 @@ require 'openshift/project_resource'
 module BushSlicer
   class CustomResourceDefinition < ClusterResource
     RESOURCE = "customresourcedefinitions"
+
+    # @return [BushSlicer::ResultHash] with :success depending on 
+    # status phase=Succeeded and reason=InstallSucceeded;
+    def ready?(user:, quiet: false, cached: false)
+      res = get(user: user, quiet: quiet)
+      if res[:success]
+        res[:success] =
+          res[:parsed]["status"] &&
+          res[:parsed]["status"]["conditions"] &&
+          res[:parsed]["status"]["conditions"].any? { |c|
+            c["type"] == "Established" && c["status"] == "True"
+          }
+      end
+      return res
+    end
   end
 end
