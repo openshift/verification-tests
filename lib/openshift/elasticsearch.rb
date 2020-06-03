aacca81587b8767ff75beb8f63033596ee727b6c
QiaolingTang <qitang@redhat.com>
2020-06-03 13:18:56 +0800

    auto ES6 cases

--- lib/openshift/elasticsearch.rb
+++ lib/openshift/elasticsearch.rb
@@ -14,15 +14,15 @@ module BushSlicer
       return rr.dig('spec', 'redundancyPolicy')
     end
 
-    def index_management(user: nil, quiet: false, cached: false)
+    def index_management(user: nil, quiet: false, cached: true)
       return raw_resource(user: user, cached: cached, quiet: quiet).dig('spec', 'indexManagement')
     end
 
-    def index_management_mappings(user: nil, quiet: false, cached: false)
+    def index_management_mappings(user: nil, quiet: false, cached: true)
       return index_management(user: user, cached: cached, quiet: quiet).dig('mappings')
     end
 
-    def mapping(user: nil, name:, quiet: false, cached: false)
+    def mapping(user: nil, name:, quiet: false, cached: true)
       mapping = nil
       mappings = self.index_management_mappings(user: user, cached: cached, quiet: quiet)
       mappings.each do | m |
@@ -31,17 +31,29 @@ module BushSlicer
       return mapping
     end
 
-    def index_management_policies(user: nil, quiet: false, cached: false)
+    def policy_ref(user: nil, name:, quiet: false, cached: true)
+      return mapping(user: user, name: name, quiet: quiet, cached: cached).dig('policyRef')
+    end
+
+    def index_management_policies(user: nil, quiet: false, cached: true)
       return index_management(user: user, cached: cached, quiet: quiet).dig('policies')
     end
 
-    def policy(user: nil, name:, quiet: false, cached: false)
+    def policy(user: nil, name:, quiet: false, cached: true)
       policy = nil
       policies = self.index_management_policies(user: user, cached: cached, quiet: quiet)
       policies.each do | p |
         policy = p if p['name'] == name
       end
       return policy
+    end
+
+    def delete_min_age(user: nil, name:, quiet: false, cached: true)
+      return policy(user: user, name: name, quiet: quiet, cached: cached).dig('phases', 'delete', 'minAge')
+    end
+
+    def rollover_max_age(user: nil, name:, quiet: false, cached: true)
+      return policy(user: user, name: name, quiet: quiet, cached: cached).dig('phases', 'hot', 'actions', 'rollover', 'maxAge')
     end
 
     def nodes(user: nil, quiet: false, cached: false)
