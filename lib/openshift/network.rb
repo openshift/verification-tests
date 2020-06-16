4b3e13c6cc48fdecb4bb9f264bd95c7824483263
Ross Brattain <rbrattai@redhat.com>
2020-06-16 00:15:38 -0400

    add network resource for operator and config

--- lib/openshift/network.rb
+++ lib/openshift/network.rb
@@ -1,14 +1,35 @@
 require 'openshift/cluster_resource'
 
 module BushSlicer
-  # @note represents an OpenShift environment Storage Class
+  # @note represents an OpenShift environment Network class
   class Network < ClusterResource
-    RESOURCE = "networks"
 
+    def cluster_network(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('spec', 'clusterNetwork')
+    end
+
+  end
+
+  class NetworkOperator < Network
+    RESOURCE = "networks.operator.openshift.io"
+    def network_type(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('spec', 'defaultNetwork', 'type')
+    end
+  end
+
+  class NetworkConfig < Network
+    RESOURCE = "networks.config.openshift.io"
     def network_type(user: nil, cached: true, quiet: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
       rr.dig('spec', 'networkType')
     end
+    def cluster_network_mtu(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('status', 'clusterNetworkMTU')
+    end
+
 
   end
 end
