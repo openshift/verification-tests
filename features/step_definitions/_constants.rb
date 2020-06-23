f61c07a9722c5c77c4cd845e4bf45eef2ea35fd4
Ross Brattain <rbrattai@redhat.com>
2020-06-23 15:51:59 -0400

    add negative look-ahead to for %> re
    negative lookahead help the `.` not match
    and should decrease back-tracking.

--- features/step_definitions/_constants.rb
+++ features/step_definitions/_constants.rb
@@ -4,8 +4,8 @@ OPT_QUOTED = '(?: "([^"\n]+)")?'
 QUOTED = '"([^"\n]+)"'
 HTTP_URL = '(https?:\/\/.+)'
 USER = 'the( \\S+)? user'
-NUMBER = '([0-9]+|<%=.+?%>)'
-WORD = '(\w+|<%=.+?%>)'
+NUMBER = '([0-9]+|<%=(?!%>).+?%>)'
+WORD = '(\w+|<%=(?!%>).+?%>)'
 OPT_WORD = "(?: #{WORD})?"
 # use for regular expression
 RE='/([^/\n]+)/'
