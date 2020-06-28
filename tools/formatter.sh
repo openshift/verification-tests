7687b36bada06fd2f3073fe5ea9aa81eb992fd8a
Liang Xia <lxia@redhat.com>
2020-06-29 00:12:22 +0800

    Add option no-snippets to formatter.sh (#1292)

--- tools/formatter.sh
+++ tools/formatter.sh
@@ -4,6 +4,6 @@ files=$@
 
 for file in $files ; do
 	echo "Formattting file: $file"
-	bundle exec cucumber --dry-run --no-color --no-profile --no-source "$file" | sed '1d' | head --lines=-3 > "${file}.bak"
+	bundle exec cucumber --dry-run --no-color --no-profile --no-snippets --no-source "$file" | sed '1d' | head --lines=-3 > "${file}.bak"
 	mv "${file}.bak" "$file"
 done
