ab0655be280ccf82558f509e078bdec4f7baebac
Hongyan Li <hongyli@redhat.com>
2020-09-01 16:28:29 +0800

    add methods for resource

--- lib/openshift/thanos_ruler.rb
+++ lib/openshift/thanos_ruler.rb
@@ -3,5 +3,10 @@ require 'openshift/project_resource'
 module BushSlicer
   class ThanosRuler < ProjectResource
     RESOURCE = "thanosrulers.monitoring.coreos.com"
+
+    def log_level(user: nil, cached: true, quiet: false)
+      raw_resource(user: user, cached: cached, quiet: quiet).dig("spec", "logLevel")
+    end
+
   end
 end
