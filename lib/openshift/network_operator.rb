cb15b27e8a322e1915df8ddd58efc2b1360b7ede
Ross Brattain <rbrattai@redhat.com>
2020-09-04 04:11:42 -0400

    SDN-655 SystemOVS: update tests to use system OVS (#1252)
    * added network object support
    
    * add network resource for operator and config
    
    * SDN-655 SystemOVS: update tests to use system OVS
    
    restarting the OVS pod should do nothing because
    OVS is running as a systemd unit on the host
    
    Use SystemdService.new("openvswitch.service", _host)
    to manipulate the OVS services
    
    Co-authored-by: Peter Ruan <pruan@redhat.com>

---
+++ lib/openshift/network_operator.rb
@@ -0,0 +1,26 @@
+require 'openshift/cluster_resource'
+
+module BushSlicer
+  # @note represents an OpenShift environment Network class
+
+  class NetworkOperator < ClusterResource
+    # full network.operator.openshift.io name doesn't seem to work,
+    # RuntimeError: hash not from a NetworkOperator: expected  but was Network
+    # raise "hash not from a #{shortclass}: expected #{kind} but was #{hash["kind"]}"
+    # api_name_no_dots = "operatoropenshiftio"
+    # resource_without_dots = "networksoperatoropenshiftio"
+    # lib/openshift/config_imageregistry_operator_openshift_io.rb` - for objects without unique name and you need full qualified name
+    RESOURCE = "network.operator"
+
+    def network_type(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('spec', 'defaultNetwork', 'type')
+    end
+
+    def cluster_network(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      rr.dig('spec', 'clusterNetwork')
+    end
+  end
+
+end
