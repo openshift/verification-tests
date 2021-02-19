e269bfd8d1bef94da4eea840c6b226899d562656
yapei <yapei@redhat.com>
2021-02-19 19:03:26 +0800

    add feature for OCP-33402

--- lib/openshift/cluster_operator.rb
+++ lib/openshift/cluster_operator.rb
@@ -11,5 +11,12 @@ module BushSlicer
       res = self.versions.select { |v| v == version }
       res.count != 0
     end
+
+    # return an array of related objects
+    def related_objects(user: nil, cached: true, quiet: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      return rr.dig('status', 'relatedObjects')
+    end
+
   end
 end
