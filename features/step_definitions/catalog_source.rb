0c745fda9413a4adc8a60546c2953174ef4e682a
Peter Ruan <pruan@redhat.com>
2021-03-05 23:52:02 -0800

    allow metering automation to run with other catalogsrc other than qe-app-registry (#1891)
    Currently, the metering automation has the restriction that catalog `qe-app-registry` must present in the cluster.  This is an unreasonable restriction.  With this PR, we use the next avilable catalogsource if qe-app-registry is not there.

--- features/step_definitions/catalog_source.rb
+++ features/step_definitions/catalog_source.rb
@@ -3,7 +3,6 @@ Given(/^admin creates "([^"]*)" catalog source with image "([^"]*)"(?: with disp
   ensure_admin_tagged
   step %Q/I switch to cluster admin pseudo user/
   step %Q/I use the "openshift-marketplace" project/
-  
   step %Q/admin ensures "#{cs_name}" catalog_source is deleted from the "openshift-marketplace" project after scenario/
   step %Q/I process and create:/, table(%{
     | f | #{BushSlicer::HOME}/testdata/olm/catalogsource-template.yaml |
@@ -16,4 +15,11 @@ Given(/^admin creates "([^"]*)" catalog source with image "([^"]*)"(?: with disp
     | olm.catalogSource=#{cs_name} |
   })
   step %Q/I wait for the "#{cs_name}" catalog_source to become ready up to 600 seconds/
+end
+
+# save the appropriate catalog name for the target operator to a clipboard for
+# future reference
+Given /^I save the catalogsource for #{QUOTED} operator to the#{OPT_SYM} clipboard$/ do |op_name, cb_name |
+  cb_name ||= :catalog
+  cb[cb_name] = package_manifest(op_name).catalog_source
 end
