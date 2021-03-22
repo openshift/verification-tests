9af609bf9fa30827ad8737741b81c867d997785b
Ya Dan Pei <yapei@redhat.com>
2021-03-22 16:15:52 +0800

    rules added for OCP-39596 (#1904)

---
+++ lib/openshift/console_operator.rb
@@ -0,0 +1,12 @@
+require 'openshift/cluster_resource'
+
+module BushSlicer
+  class ConsoleOperator < ClusterResource
+    RESOURCE = 'console.operator'
+
+	  def plugins(user: nil, cached: false, quiet: false)
+      spec = raw_resource(user: user, cached: cached, quiet: quiet).dig('spec')
+      spec.dig('plugins')
+	  end
+  end
+end
