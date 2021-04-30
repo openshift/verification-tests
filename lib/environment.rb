0afd68ed58253323109fc3caa34ee405b2463a9a
Qin Ping <piqin@redhat.com>
2021-04-30 08:15:03 +0000

    Add supports if the default scheduler is set

--- lib/environment.rb
+++ lib/environment.rb
@@ -438,7 +438,10 @@ module BushSlicer
     #   support purposes (e.g. host a debug pod for running node commands)
     def service_project
       unless @service_project
-        project = Project.new(name: "tests-" + EXECUTOR_NAME.downcase, env: self)
+        # if the cluster set the default scheduler, set the project running debug pod node-selector=''
+        # to overwrite the default scheduler, or the pod can not be run successfully
+        project_name = "tests-" + EXECUTOR_NAME.downcase
+        project = Project.new(name: project_name, env: self)
         unless project.active?
           # 30 seconds is no longer enough
           project.wait_to_disappear(admin, 60)
@@ -446,6 +449,7 @@ module BushSlicer
           unless res[:success]
             raise "failed to create service project #{project.name}, see log"
           end
+          admin.cli_exec(:annotate, resource: "namespace", resourcename: project_name, keyval: 'openshift.io/node-selector=', overwrite: true)
           # we must update the cache, since we just waited for the previously active project to disappear
           project.reload
         end
