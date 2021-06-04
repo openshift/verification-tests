8d4c481126d74bc48e9bb1fa3796102875275471
Peter Ruan <pruan@redhat.com>
2021-06-04 15:43:09 -0700

    corrected grammar
    Given there is 1 "daemon_set" replica in the "openshift-monitoring"
    project
    Given there are 2 "daemon_set" replicas in the "openshift-monitoring"
    project

--- features/step_definitions/replicas.rb
+++ features/step_definitions/replicas.rb
@@ -2,7 +2,7 @@
 #
 
 ## example usage Given there are 6 "daemon_set" replicas in the "openshift-monitoring" project
-Given /^there are #{NUMBER} #{QUOTED} replicas in the #{QUOTED} project$/ do |expected, resource_type, namespace|
+Given /^there (is|are) #{NUMBER} #{QUOTED} replicas? in the #{QUOTED} project$/ do |pronoun, expected, resource_type, namespace|
   ensure_admin_tagged
   expected = expected.to_i
   clazz = resource_class(resource_type)
@@ -14,7 +14,7 @@ Given /^there are #{NUMBER} #{QUOTED} replicas in the #{QUOTED} project$/ do |ex
   list.each do | cl |
     desired_count = cl.replica_counters(cached: false)[:desired]
     if desired_count != expected.to_i
-      raise "#{resource_type} count is #{desired_count}, but is expected to be #{expected} instead"
+      raise "#{resource_type} #{cl.name} count is #{desired_count}, but is expected to be #{expected} instead"
     end
   end
 end
