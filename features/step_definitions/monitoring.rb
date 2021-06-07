150435ac36fe87dda2d20f04ea7396bbc88f1ee4
Hongyan Li <hongyli@redhat.com>
2021-06-07 09:30:51 +0800

    change are as is according to review comments

--- features/step_definitions/monitoring.rb
+++ features/step_definitions/monitoring.rb
@@ -2,12 +2,12 @@ Then(/^I check replicas of monitoring components for sno cluster$/) do
 
   if infrastructure("cluster").infra_topology=="SingleReplica"
 
-    step %Q/there are 1 "daemonset" replicas in the "openshift-monitoring" project/
-    step %Q/there are 1 "statefulset" replicas in the "openshift-monitoring" project/
-    step %Q/there are 1 "deployment" replicas in the "openshift-monitoring" project/
-    step %Q/there are 1 "daemonset" replicas in the "openshift-monitoring" project/
-    step %Q/there are 1 "statefulset" replicas in the "openshift-user-workload-monitoring " project/
-    step %Q/there are 1 "deployment" replicas in the "openshift-user-workload-monitoring" project/
+    step %Q/there is 1 "daemonset" replicas in the "openshift-monitoring" project/
+    step %Q/there is 1 "statefulset" replicas in the "openshift-monitoring" project/
+    step %Q/there is 1 "deployment" replicas in the "openshift-monitoring" project/
+    step %Q/there is 1 "daemonset" replicas in the "openshift-monitoring" project/
+    step %Q/there is 1 "statefulset" replicas in the "openshift-user-workload-monitoring " project/
+    step %Q/there is 1 "deployment" replicas in the "openshift-user-workload-monitoring" project/
 
   end
 end
