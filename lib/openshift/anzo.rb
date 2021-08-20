ceb7164e45040aa703bc3e077adb19a5f4fc7fe9
Ya Dan Pei <yapei@redhat.com>
2021-08-20 18:07:34 +0800

    update support rules (#2208)

---
+++ lib/openshift/anzo.rb
@@ -0,0 +1,7 @@
+require 'openshift/project_resource'
+
+module BushSlicer
+  class Anzo < ProjectResource
+    RESOURCE = "anzos"
+  end
+end
--- lib/openshift/subscription.rb
+++ lib/openshift/subscription.rb
@@ -7,6 +7,11 @@ module BushSlicer
       return rr.dig('status', 'currentCSV')
     end
 
+    def startingCSV(user: nil, quiet: false, cached: false)
+      rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      return rr.dig('status', 'currentCSV')      
+    end
+
     def ready?(user:, quiet: false)
        res = get(user: user, quiet: quiet)
        if res[:success]
