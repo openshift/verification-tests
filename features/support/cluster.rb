01390b0b72810c5f604c1ccbe7f44163516a4e36
Vadim Aliev <valiev@redhat.com>
2021-09-12 10:41:19 +0300

    cluster and node object file
    Those files can be used by Openshift QE, regardless specifications

---
+++ features/support/cluster.rb
@@ -0,0 +1,82 @@
+class ClusterObj
+#Class for openshift cluster object, to be used in tests
+  include BushSlicer
+  require "world"
+  require 'json'
+  #attr_reader :uri, :token
+  attr_accessor :data, :nodeAmmount, :nodes, :workerNodesAmmount, :masterNodesAmmount, :uri, :logger, :version, :clusterEnv, :randomWorkerNodeName, 
+		:kataconfigsList, :podsList, :namespacesList, :fipsStatus, :nodeNamesList, :nodesList
+
+  def initialize()
+#myworld: cucushift default class type:bushslicer default class object
+#logger: cucushift logs class type:bushslicer default log class object
+#admin: cli class type: bushslicer default cli class object
+#uri: cluster host
+# uri type: string
+#version: openshift cluster version type: string
+#data: cluster nodes info type:json
+#clusterEnv: cluster type (e.g. GCP, AZURE) type: string
+#nodeAmmount: total cluster nodes amount type: int
+#workerNodesAmmount: worker nodes amount type:int
+#masterNodesAmmount: master nodes amount typr: int
+#randomWorkerNodeName: random worker node name type: string
+#nodeNamesList: list of cluster node names type: array
+#nodesList: list of cluster node objects type: NodeObj
+#TODO convert namespacesList to array
+#namespacesList: list of cluster namespaces type: string
+#TODO convert kataconfigsList to array
+#kataconfigsList: list of cluster kataconfigs type: string
+#podsList:l list of cluster pods type: array
+#fipsStatus: fips enabled("1")/disabled("0") type: string
+
+        myworld = DefaultWorld.new()
+	@logger = Logger.new()
+	@admin = myworld.admin
+        myworld.project(KATA_NAMESPACE)
+        @uri = @admin.cli_exec(:get, resource: "infrastructure", o: "jsonpath='{..status.apiServerURL}'")[:stdout]
+	@version = getClusterVersion()
+	@datajs = @admin.cli_exec(:get, resource: "nodes", o: "json")
+ 	@data = JSON.parse(@datajs[:stdout])
+  	@clusterEnv = @admin.cli_exec(:get, resource: "infrastructure", o: "jsonpath='{..status.platform}'")[:stdout]
+  	@nodeAmmount = @data['items'].count
+  	@workerNodesAmmount = 0
+  	@masterNodesAmmount = 0
+	@randomWorkerNodeName = ""
+	@nodeNamesList = (@admin.cli_exec(:get, resource: "nodes", o: "jsonpath='{..metadata.name}'")[:stdout]).split(" ")
+	@nodesList = getNodeObjects()
+	getNodesType()
+	@namespacesList = @admin.cli_exec(:get, resource: "namespace")[:stdout]
+	@kataconfigsList = @admin.cli_exec(:get, resource: "kataconfig")[:stdout]
+	@podsList = (@admin.cli_exec(:get, resource: "pods")[:stdout]).split(" ")
+	@fipsStatus = getFipsStatus(@randomWorkerNodeName)
+  end; nil
+  
+  def getNodesType()
+#Counting master and nodes amount and assigns random worker node name
+#parameters: none
+#return: none
+	 self.nodeNamesList.each do |node|
+		node = node.delete("'")
+    		if node.to_s.include? 'master'
+      			self.masterNodesAmmount += 1
+    		else
+			self.randomWorkerNodeName = node
+      			self.workerNodesAmmount += 1
+    		end
+  	end
+  end;nil
+  
+  def getNodeObjects()
+#Creating list of node objects
+#parameters: none
+#return: nodeList type: list of NodeObj
+
+	nodesList = []
+	self.nodeNamesList.each do |node|
+		node = node.delete("'")
+        	nodeObj = NodeObj.new(node)
+        	nodesList.push(nodeObj)
+        end
+   	return nodesList
+  end
+end; nil
---
+++ features/support/node.rb
@@ -0,0 +1,58 @@
+class NodeObj
+#Class for node object
+  include BushSlicer
+  require "world"
+  require 'json'
+  attr_reader :name
+  attr_accessor :name, :uid, :labels, :networkUnavailable, :memoryPressure, :diskPressure, :pIDPressure, :ready, :conditions, :datajs, :data
+
+  def initialize(name)
+#name: node name type: string
+#myworld: cucushift default class type:bushslicer default class object
+#logger: cucushift logs class type:bushslicer default log class object
+#admin: cli class type: bushslicer default cli class object
+#uid: node UDI type: string
+#labels: node labels type: array
+#networkUnavailable: network node status type: string
+#memoryPressure: memory node status type: string
+#diskPressure: memory node status type: string
+#pIDPressure: memory node status type: string
+#ready: overall node health status type: string
+#data: data dictionary for node conditions type: json
+        myworld = DefaultWorld.new()
+	@admin = myworld.admin
+        myworld.project(KATA_NAMESPACE)
+	@name = name
+	@uid = (@admin.cli_exec(:get, resource: "nodes/#{@name}", o: "jsonpath='{.metadata.uid}'")[:stdout])
+	@labels = (@admin.cli_exec(:get, resource: "nodes/#{@name}", o: "jsonpath='{.metadata.labels}'")[:stdout]).split(" ")
+	@networkUnavailable = "NetworkUnavailable"
+	@memoryPressure = "MemoryPressure"
+	@diskPressure = "DiskPressure"
+	@pIDPressure = "PIDPressure"
+	@ready = "Ready"
+	@conditions = @admin.cli_exec(:get, resource: "nodes/#{@name}", o: "jsonpath='{..status.conditions}'")[:stdout]
+ 	@conditions = @conditions.delete("'")
+ 	@data = JSON.parse(@conditions)
+	getNodesConditions()
+
+  end; nil
+  
+  def getNodesConditions()
+#Collecting conditions for node object
+#parameter: none
+#return: none
+	 self.data.each do |node|
+    		if node['type'].equal? self.networkUnavailable
+      			self.networkUnavailable = node['status'] 
+    		elsif node['type'].equal? self.memoryPressure
+			self.memoryPressure = node['status']
+		elsif node['type'].equal? self.diskPressure    
+                        self.diskPressure = node['status']
+		elsif node['type'].equal? self.pIDPressure 
+                        self.pIDPressure = node['status']
+		elsif node['type'].equal? self.ready
+                        self.ready = node['status']
+		end
+    	end
+  end
+end; nil
