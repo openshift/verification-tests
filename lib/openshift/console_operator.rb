10ee2e12f46f2f40e129aaa9bba41626f504fc2d
liangxia <lxia@redhat.com>
2021-10-15 17:08:07 +0800

    Multiple small fixes

--- lib/openshift/console_operator.rb
+++ lib/openshift/console_operator.rb
@@ -4,9 +4,9 @@ module BushSlicer
   class ConsoleOperator < ClusterResource
     RESOURCE = 'console.operator'
 
-	  def plugins(user: nil, cached: false, quiet: false)
+    def plugins(user: nil, cached: false, quiet: false)
       spec = raw_resource(user: user, cached: cached, quiet: quiet).dig('spec')
       spec.dig('plugins')
-	  end
+    end
   end
 end
