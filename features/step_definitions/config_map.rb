6fa29eff019cb3bac8c31aa999226b120d2ea0c7
Peter Ruan <pruan@redhat.com>
2021-11-01 18:48:52 -0700

    get the leader node from config_map (#2417)

--- features/step_definitions/config_map.rb
+++ features/step_definitions/config_map.rb
@@ -6,3 +6,13 @@ Given /^value of #{QUOTED} in configmap #{QUOTED} as YAML is merged with:$/ do |
 
   config_map(cm_name).set_value(key, current_content.to_yaml, user: user)
 end
+
+Given /^I store the leader node name from the #{QUOTED} configmap to the#{OPT_SYM} clipboard$/ do | cm_name, cb_name |
+
+  cb_name ||= "holderIdentity"
+  rr = config_map(cm_name).raw_resource
+  leader_str = rr.dig('metadata', 'annotations', 'control-plane.alpha.kubernetes.io/leader')
+  leader_yaml = YAML.load(leader_str)
+  cb[cb_name] = leader_yaml.dig("holderIdentity")
+
+end
