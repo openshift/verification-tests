c78fa145d1b2e31146b7927b44d21047634ed6c2
liangxia <lxia@redhat.com>
2021-11-16 09:39:58 +0800

    Disable overwrite of secrets's quiet option

--- lib/openshift/secret.rb
+++ lib/openshift/secret.rb
@@ -9,19 +9,19 @@ module BushSlicer
 
     # see #raw_resource
     def type(user: nil, cached: true, quiet: true)
-      obj = raw_resource(user: user, cached: cached, quiet: quiet)
+      obj = raw_resource(user: user, cached: cached, quiet: true)
       return obj['type']
     end
 
     # @param user [BushSlicer::User] the user to run cli commands with if needed
     def bearer_token?(user: nil, cached: true, quiet: true)
-      type(user: user, cached: cached, quiet: quiet).include?('service-account-token') && raw_resource.dig('data', 'token')
+      type(user: user, cached: cached, quiet: true).include?('service-account-token') && raw_resource.dig('data', 'token')
     end
 
     # @param user [BushSlicer::User] the user to run cli commands with if needed
     # @return [String] bearer token
     def token(user: nil, cached: true, quiet: true)
-      if bearer_token?(user: user, cached: cached, quiet: quiet)
+      if bearer_token?(user: user, cached: cached, quiet: true)
         return value_of(:token)
       else
         raise "secret #{name} does not contain a token"
@@ -29,12 +29,12 @@ module BushSlicer
     end
 
     def raw_value_of(key, user: nil, cached: true, quiet: true)
-      obj = raw_resource(user: user, cached: cached, quiet: quiet)
+      obj = raw_resource(user: user, cached: cached, quiet: true)
       obj.dig("data", key.to_s)
     end
 
     def value_of(key, user: nil, cached: true, quiet: true)
-      value = raw_value_of(key, user: user, cached: cached, quiet: quiet)
+      value = raw_value_of(key, user: user, cached: cached, quiet: true)
       value ? Base64.decode64(value) : nil
     end
   end
