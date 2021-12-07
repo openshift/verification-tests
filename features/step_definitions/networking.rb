8237cb6a8b2c230217853a9752c7d61453b4b2ae
LiangquanLi930 <liangli@redhat.com>
2021-12-07 16:31:29 +0800

    update ip-echo image

--- features/step_definitions/networking.rb
+++ features/step_definitions/networking.rb
@@ -586,7 +586,7 @@ Given /^an IP echo service is setup on the master node and the ip is stored in t
   cb_name = "ipecho_ip" unless cb_name
   cb[cb_name] = host.local_ip
 
-  @result = host.exec_admin("docker run --name ipecho -d -p 8888:80 quay.io/openshifttest/ip-echo")
+  @result = host.exec_admin("docker run --name ipecho -d -p 8888:80 quay.io/openshifttest/ip-echo:multiarch")
   raise "Failed to create the IP echo service." unless @result[:success]
   teardown_add {
     @result = host.exec_admin("docker rm -f ipecho")
