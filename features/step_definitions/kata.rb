03192da986432aec34edbf12b47c9b618d59526e
Vadim Aliev <valiev@redhat.com>
2021-12-20 16:55:11 +0200

    Update kata.rb

--- features/step_definitions/kata.rb
+++ features/step_definitions/kata.rb
@@ -33,6 +33,9 @@ When /^i install sandboxed-operator in #{QUOTED} namespace$/ do |kata_ns|
   step %Q|I obtain test data file "kata/operatorgroup.yaml"|
   @result = user.cli_exec(:apply, f: "operatorgroup.yaml")
   raise "Failed to install sandboxed-operator" unless @result[:success]
+  step %Q|I obtain test data file "kata/catalogsource.yaml"|
+  @result = user.cli_exec(:apply, f: "catalogsource.yaml")
+  raise "Failed to install sandboxed-operator" unless @result[:success]
   step %Q|I obtain test data file "kata/subscription.yaml"|
   @result = user.cli_exec(:apply, f: "subscription.yaml")
   raise "Failed to install sandboxed-operator" unless @result[:success]
