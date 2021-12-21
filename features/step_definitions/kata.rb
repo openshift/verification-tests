0fc3901e293affbca5db09d2e84e4f7dfb691629
Vadim Aliev <valiev@redhat.com>
2021-12-21 13:00:23 +0200

    Update kata.rb

--- features/step_definitions/kata.rb
+++ features/step_definitions/kata.rb
@@ -57,8 +57,8 @@ Given /^I wait until sandboxed operator is ready$/ do
   operator_status[:success] = wait_for(timeout, stats: {}) do
     step %Q/I store master major version in the :master_version clipboard/
     if cb.master_version == "4.8"
-      @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[8].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_status
-      @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[9].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_state
+      @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[7].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_status
+      @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[8].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_state
     else
       @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[4].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_status
       @result = admin.cli_exec(:get, resource: "operators", o: "jsonpath=''{..status.components.refs[5].conditions[0].type}'", n:kata_ns)[:stdout].to_s.include? expected_state
