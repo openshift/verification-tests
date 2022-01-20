c9ee621d9116d83329fc4972363f989437be0654
Liang Xia <lxia@redhat.com>
2022-01-20 22:41:57 +0800

    Fix for OCP-19695

--- lib/openshift/horizontal_pod_autoscaler.rb
+++ lib/openshift/horizontal_pod_autoscaler.rb
@@ -13,22 +13,28 @@ module BushSlicer
 
     def target_cpu_utilization_percentage(user: nil, cached: true, quiet: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      cpu_pct = rr.dig('spec', 'targetCPUUtilizationPercentage')
+      return cpu_pct.to_i unless cpu_pct.nil?
+
       metrics = rr.dig('spec', 'metrics')
-      cpu = metrics.find { |metric|
+      metric = metrics.find { |metric|
         metric.dig('resource', 'name') == 'cpu'
-        return metric.dig('resource', 'target', 'averageUtilization')
-      } unless metrics.nil? || rr.dig('spec', 'targetCPUUtilizationPercentage')
-      return cpu.to_i
+      } unless metrics.nil?
+      avg_utl = metric.dig('resource', 'target', 'averageUtilization') unless metric.nil?
+      return avg_utl.to_i
     end
 
     def current_cpu_utilization_percentage(user: nil, cached: true, quiet: false)
       rr = raw_resource(user: user, cached: cached, quiet: quiet)
+      cpu_pct = rr.dig('status', 'currentCPUUtilizationPercentage')
+      return cpu_pct.to_i unless cpu_pct.nil?
+
       metrics = rr.dig('status', 'currentMetrics')
-      cpu = metrics.find { |metric|
+      metric = metrics.find { |metric|
         metric.dig('resource', 'name') == 'cpu'
-        return metric.dig('resource', 'current', 'averageUtilization')
-      } unless metrics.nil? || rr.dig('status', 'currentCPUUtilizationPercentage')
-      return cpu.to_i
+      } unless metrics.nil?
+      avg_utl = metric.dig('resource', 'current', 'averageUtilization') unless metric.nil?
+      return avg_utl.to_i
     end
   end
 end
