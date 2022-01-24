438bd77cb7697cf2216dbcc2f0953e98fc9aa92c
Peter Ruan <pruan@redhat.com>
2022-01-23 19:18:11 -0800

    added summary for AWS-China (#2643)
    * added summary for AWS-China
    
    * use a awscred that reads the correct usage data
    
    depends on https://gitlab.cee.redhat.com/aosqe/cucushift-internal/-/merge_requests/24

--- tools/cloud_usage.rb
+++ tools/cloud_usage.rb
@@ -56,10 +56,16 @@ module BushSlicer
         c.option("-r", "--region region_name", "report on this region only")
         c.option("-u", "--uptime cluter uptime limit", "report for clusters having uptime over this limit")
         c.action do |args, options|
-          ps = AwsSummary.new(jenkins: @jenkins)
+          ps = AwsSummary.new(svc_name: :"AWS-CLOUD-USAGE", jenkins: @jenkins)
           options.config = conf
           say 'Getting summary...'
           ps.get_summary(target_region: options.region, options: options)
+          # add support for AWS-CHINA regions
+          global_region = :"AWS-CI-CHINA"
+          ps = AwsSummary.new(svc_name: global_region ,jenkins: @jenkins)
+          options.config = conf
+          say 'Getting summary...'
+          ps.get_summary(target_region: options.region, options: options, global_region: global_region)
 
         end
       end
