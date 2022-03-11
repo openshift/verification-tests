d75d6fee07350e94a3a0c90a812acccf20658260
Weibin Liang <weliang@redhat.com>
2022-03-10 22:08:17 -0500

    Wait for 120 seconds for checking multus status (#2726)
    * Wait for 120 seconds for checking multus status
    
    * Update check multus steps
    
    * Delete unuseful lines of code

--- features/step_definitions/networking.rb
+++ features/step_definitions/networking.rb
@@ -595,23 +595,21 @@ Given /^an IP echo service is setup on the master node and the ip is stored in t
   }
 end
 
-
 Given /^the multus is enabled on the cluster$/ do
   ensure_admin_tagged
-
-  desired_multus_replicas = daemon_set('multus', project('openshift-multus')).replica_counters(user: admin)[:desired]
-  available_multus_replicas = daemon_set('multus', project('openshift-multus')).replica_counters(user: admin)[:available]
-  #storing desired_multus_replicas value in desired_multus_replicas clipboard variable as well
-  cb.desired_multus_replicas = desired_multus_replicas
-  unless (desired_multus_replicas == available_multus_replicas || desired_multus_replicas > env.nodes.count) && available_multus_replicas != 0
-    daemon_set('multus', project('openshift-multus')).describe(admin, quiet:false)
-    BushSlicer::Pod.get_labeled("app=multus", user: admin, project: project("openshift-multus", switch: false)) do |pod|
-      pod.describe(admin, quiet: false)
+  success = wait_for(120, interval: 10)  {
+    desired_multus_replicas = daemon_set('multus', project('openshift-multus')).replica_counters(user: admin)[:desired]
+    available_multus_replicas = daemon_set('multus', project('openshift-multus')).replica_counters(user: admin)[:available]
+    if (desired_multus_replicas == available_multus_replicas || desired_multus_replicas > env.nodes.count) && available_multus_replicas != 0 
+      true
+    else
+      logger.info("Multus is not running correctly, continue checking")
+      false
     end
-    env.nodes(user:admin, refresh: true, quiet: false)
-    raise "Multus is not running correctly!"
+  } 
+  unless success
+    raise "Multus is not running correctly! Exit Testing" 
   end
-
 end
 
 Given /^the status of condition#{OPT_QUOTED} for network operator is :(.+)$/ do | type, status |
