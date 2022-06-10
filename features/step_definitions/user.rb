aeaa3d06c228520f3543ef2ad7b412ed201c91d3
Xingxing Xia <xxia@redhat.com>
2022-06-10 14:45:49 +0800

    Fix serviceaccount 'RuntimeError: no api accessors for user'

--- features/step_definitions/user.rb
+++ features/step_definitions/user.rb
@@ -24,6 +24,13 @@ end
 
 Given /^I find a bearer token of the(?: (.+?))? service account$/ do |acc|
   service_account(acc).load_bearer_tokens(user: user)
+  if service_account(acc).cached_tokens == []
+    # Starting with kube 1.24, i.e. OCP 4.11, serviceaccount's YAML does not have an automatically-generated secret-based token
+    # So we explicitly create token here
+    @result = user.cli_exec(:create_token, [[:serviceaccount, acc]])
+    raise "Could not create token for the serviceaccount #{acc}" unless @result[:success]
+    service_account(acc).add_str_token(@result[:response], protect: true)
+  end
 end
 
 Given /^the(?: ([a-z]+))? user has all owned resources cleaned$/ do |who|
