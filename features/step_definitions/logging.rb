33b6a50809b7c7485e9b19ae76503c088d36fee5
Qiaoling Tang <qitang@redhat.com>
2022-07-07 14:31:09 +0800

    skip timeout error in fluentd

--- features/step_definitions/logging.rb
+++ features/step_definitions/logging.rb
@@ -1353,6 +1353,8 @@ Given /^I (check|record) all pods logs in the#{OPT_QUOTED} project(?: in last (\
           log = check_log(@result[:response], error_strings, ["can't remove non-existent inotify watch for"])
         when "kibana"
           log = check_log(@result[:response], error_strings, ["java.lang.UnsupportedOperationException"])
+        when "collector"
+          log = check_log(@result[:response], error_strings, ["Timeout flush: kubernetes.var.log"])
         else
           log = check_log(@result[:response], error_strings, nil)
         end
