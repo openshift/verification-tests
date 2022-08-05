4e2ddcd1224ffd56917b5edb035be49bb3958601
Liang Xia <lxia@redhat.com>
2022-08-05 09:10:13 +0800

    Project name must be no more than 63 chars (#2986)

--- lib/bushslicer.rb
+++ lib/bushslicer.rb
@@ -61,7 +61,7 @@ module BushSlicer
   # EXECUTOR_NAME is used as part of project name and workdir,
   # truncate it as the project name is limited to 63 chars,
   # apppend "x" to make sure it always ends with proper char
-  EXECUTOR_NAME = "#{EXECUTOR_NAME_TEMP[0..55]}x".freeze
+  EXECUTOR_NAME = "#{EXECUTOR_NAME_TEMP[0..50]}x".freeze
 
   START_TIME = Time.now
   TIME_SUFFIX = [
