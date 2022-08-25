90a11bf75f56a5671a2355a98dd71d1e3dc3074e
Xingxing Xia <xxia@redhat.com>
2022-08-25 09:38:45 +0800

    Add warn and audit labels to avoid alert (#3004)

--- features/step_definitions/helper_services.rb
+++ features/step_definitions/helper_services.rb
@@ -505,10 +505,8 @@ Given /^I have a iSCSI setup in the environment$/ do
     @result = admin.cli_exec(:create_namespace, name: 'iscsi-target')
     raise 'failed to create "iscsi-target" project' unless @result[:success]
     if env.version_ge("4.12", user: user)
-      @result = admin.cli_exec(:label, resource: 'ns/iscsi-target', key_val: 'security.openshift.io/scc.podSecurityLabelSync=false', overwrite: 'true')
-      raise 'failed to add label "security.openshift.io/scc.podSecurityLabelSync=false" to "iscsi-target" namespace' unless @result[:success]
-      @result = admin.cli_exec(:label, resource: 'ns/iscsi-target', key_val: 'pod-security.kubernetes.io/enforce=privileged', overwrite: 'true')
-      raise 'failed to add label "pod-security.kubernetes.io/enforce=privileged" to "iscsi-target" namespace' unless @result[:success]
+      @result = admin.cli_exec(:label, [[:resource, "ns/iscsi-target"], [:overwrite, "true"], [:key_val, "security.openshift.io/scc.podSecurityLabelSync=false"], [:key_val, "pod-security.kubernetes.io/enforce=privileged"], [:key_val, "pod-security.kubernetes.io/audit=privileged"], [:key_val, "pod-security.kubernetes.io/warn=privileged"]])
+      raise 'failed to add pod security labels to "iscsi-target" namespace' unless @result[:success]
     end
   end
 
